075cf33bfd4e280141642d6edadf6280fee96661
globe22 <jebon0722@gmail.com>
2018-01-30 22:54:47 +0900

    Map을 이용해 더 간결하게 리스트 만들기!!

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
+//메인컴포넌트가 데이터 가짐.
 const movieTitles = [
   "Matrix",
   "Full Metal Jacket",
@@ -17,15 +18,33 @@ const movieImages = [
   "http://imgc.allpostersimages.com/img/posters/star-wars-the-force-awakens-one-sheet_u-L-F8G58H0.jpg",
 ]
 
+const movies = [
+{
+  title:"Matrix",
+  poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
+},
+{
+  title:"Full Metal Jacket",
+  poster:"https://images-na.ssl-images-amazon.com/images/I/81XARapmq6L._RI_SX200_.jpg",
+},
+{
+  title:"Oldboy",
+  poster:"https://images-na.ssl-images-amazon.com/images/I/91ONQ8FNHJL._SY445_.jpg",
+},
+{
+  title:"Star Wars",
+  poster:"http://imgc.allpostersimages.com/img/posters/star-wars-the-force-awakens-one-sheet_u-L-F8G58H0.jpg",
+}
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImages[0]}/> 
-        {/* MovieTitle ..라는 컴포넌트 불러오기 */}
-        <Movie title={movieTitles[1]} poster={movieImages[1]}/>
-        <Movie title={movieTitles[2]} poster={movieImages[2]}/>
-        <Movie title={movieTitles[3]} poster={movieImages[3]}/>
+        {movies.map(movie => {
+          // 리스트 작성 방법 : movies 배열 --매핑--> 새로운 배열 => 더 더 단순해졌다!
+          return <Movie title={movie.title} poster={movie.poster}/>
+        })}
       </div>
     );
   }
