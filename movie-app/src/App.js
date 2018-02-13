08663768b02d499019ecbda0b2ae667e1903819a
globe22 <jebon0722@gmail.com>
2018-02-13 22:39:22 +0900

    state의 데이터 setState에 넣기, 그래서 this.state.movies가 존재하지 않는 에러 발생! 강의를 들으며 고쳐보아요 ;)

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -9,36 +9,34 @@ class App extends Component {
   //랜더나 업데이트를 할때의 순서 : 항상 이렇게 작동!
   
   state = {
-    movies : [
-      {
-        title:"Matrix",
-        poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
-      },
-      {
-        title:"Full Metal Jacket",
-        poster:"https://images-na.ssl-images-amazon.com/images/I/81XARapmq6L._RI_SX200_.jpg",
-      },
-      {
-        title:"Oldboy",
-        poster:"https://images-na.ssl-images-amazon.com/images/I/91ONQ8FNHJL._SY445_.jpg",
-      },
-      {
-        title:"Star Wars",
-        poster:"http://imgc.allpostersimages.com/img/posters/star-wars-the-force-awakens-one-sheet_u-L-F8G58H0.jpg",
-      }
-      ]
+   
   }
 
   componentDidMount(){
    setTimeout(()=>{ //와 setTimeout(function(){는 동일, but 최신js - 옛js
     this.setState({
       movies : [
-        ...this.state.movies, //이 코드 삭제 시 아래 것으로 '대체'된다! 지금은 그대로두고, 아래코드 추가!
+        {
+          title:"Matrix",
+          poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
+        },
+        {
+          title:"Full Metal Jacket",
+          poster:"https://images-na.ssl-images-amazon.com/images/I/81XARapmq6L._RI_SX200_.jpg",
+        },
+        {
+          title:"Oldboy",
+          poster:"https://images-na.ssl-images-amazon.com/images/I/91ONQ8FNHJL._SY445_.jpg",
+        },
+        {
+          title:"Star Wars",
+          poster:"http://imgc.allpostersimages.com/img/posters/star-wars-the-force-awakens-one-sheet_u-L-F8G58H0.jpg",
+        },
         {
           title:"Coco",
           poster:"https://www.cineworld.ie/xmedia-cw/repo/feats/posters/HO00004273.jpg"
         }
-      ]
+        ]
     })
    }, 3000)
   }
@@ -46,10 +44,11 @@ class App extends Component {
   render() { // : 컴포넌트의 state가 바뀔때마다 발생됨
     return (
       <div className="App">
-        {this.state.movies.map((movie, index) => { //movie마다 index를 주고 고유key값 만들어주기! 
+        {/* {this.state.movies.map((movie, index) => { //movie마다 index를 주고 고유key값 만들어주기! 
           // 리스트 작성 방법 : movies 배열 --매핑--> 새로운 배열 => 더 더 단순해졌다!
           return <Movie title={movie.title} poster={movie.poster} key={index}/>
-        })}
+        })} error : this.state.movies가 존재하지 않아 mapping할 수 없다!
+        */} 
       </div>
     );
   }
